ab2bf89c5c4413add264f9a2a6a772e904c39d0a
Lur0321 <urielnol20@gmail.com>
2024-08-23 23:14:16 -0700

    Match y def listar

--- listas.py
+++ listas.py
@@ -11,17 +11,22 @@ def  agregar(valor, valor2 = None):
     pos = len(tareas)
   return print(f"{valor} se agrego en la posición {pos}")
 
+def listar():
+  if not tareas:
+    print("Vacio")
+  for indice, tarea in enumerate(tareas):
+    print(f"{indice + 1}.- {tarea}")
 while True:
     opList = int(input("Lista de tareas\n 1.-Agregar\n 2.-Eliminar\n 3.-Listar\n 4.-Salir\n"))
-    if opList == 1:
+    match opList:
+      case 1:
          valor = input("Agrega tu tarea\n")
          valor2_input = input(f"A que lugar deseas agregarlo? (Si solo quieres agregarla al final da ENTER)\n Tareas actuales: {len(tareas)}\n")
          valor2 = int(valor2_input) if valor2_input.strip() else None
          agregar(valor, valor2)
-    elif opList == 3:
-      if not tareas:
-        print("Vacio")
-      for indice, tarea in enumerate(tareas):
-        print(f"{indice + 1}.- {tarea}")       
-    else: 
-      break
+      case 3:
+        listar()
+      case 4:
+        break       
+      case _:
+        print("Agregue una opción valida")
